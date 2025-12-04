e0c692134f956af85180010eb2fabcf92004fab8
ktrFern <163234159+ktrFern@users.noreply.github.com>
2025-12-04 22:56:31 +0700

    Update lab2.cpp

--- os_lab2/lab2.cpp
+++ os_lab2/lab2.cpp
@@ -1,199 +1,118 @@
 #include <iostream>
 #include <cstring>
 #include <csignal>
-#include <cerrno>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/select.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
-#define PORT 3030
+using namespace std;
 
-static volatile sig_atomic_t got_sighup = 0;
+volatile sig_atomic_t wasSigHup = 0;
 
-static void sigh(int) {
-    got_sighup = 1;
+void sigHupHandler(int sig) {
+    wasSigHup = 1;
 }
 
-class ClientConnection {
-public:
-    ClientConnection() : fd(-1) {}
-
-    bool isActive() const { return fd != -1; }
-    int getFd() const { return fd; }
-    void setFd(int f) { fd = f; }
-
-    void closeClient() {
-        if (fd != -1) {
-            std::cout << "Отключение клиента (сокет " << fd << ")\n";
-            close(fd);
-            fd = -1;
-        }
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <port>" << endl;
+        return EXIT_FAILURE;
     }
-
-    void handleRead() {
-        char buf[1024];
-        ssize_t rd = read(fd, buf, sizeof(buf));
-
-        if (rd > 0) {
-            std::cout << "Получено: " << rd << " байт\n";
-        } else if (rd == 0) {
-            std::cout << "Клиент отключился (сокет " << fd << ")\n";
-            closeClient();
-        } else {
-            perror("read");
-            closeClient();
+    
+    int port = atoi(argv[1]);
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+    bind(listen_fd, (struct sockaddr*)&addr, sizeof(addr));
+    listen(listen_fd, SOMAXCONN);
+    cout << "Server started on port " << port << endl;
+    struct sigaction sa;
+    sigaction(SIGHUP, NULL, &sa);
+    sa.sa_handler = sigHupHandler;
+    sa.sa_flags |= SA_RESTART;
+    sigaction(SIGHUP, &sa, NULL);
+    sigset_t blockedMask, origMask;
+    sigemptyset(&blockedMask);
+    sigaddset(&blockedMask, SIGHUP);
+    sigprocmask(SIG_BLOCK, &blockedMask, &origMask);
+    int client_fd = -1;
+    
+    while (true) {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(listen_fd, &fds);
+        int maxFd = listen_fd;
+        
+        if (client_fd != -1) {
+            FD_SET(client_fd, &fds);
+            if (client_fd > maxFd) maxFd = client_fd;
         }
-    }
-
-private:
-    int fd;
-};
-
-class Server {
-public:
-    Server(int port) : port(port), srv(-1) {}
-
-    bool init() {
-        std::cout << "Сервер на порту " << port << "\n";
-
-        srv = socket(AF_INET, SOCK_STREAM, 0);
-        if (srv < 0) {
-            perror("socket");
-            return false;
-        }
-
-        int opt = 1;
-        if (setsockopt(srv, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-            perror("setsockopt");
-            return false;
-        }
-
-        struct sockaddr_in sa{};
-        sa.sin_family = AF_INET;
-        sa.sin_addr.s_addr = htonl(INADDR_ANY);
-        sa.sin_port = htons(port);
-
-        if (bind(srv, (struct sockaddr*)&sa, sizeof(sa)) < 0) {
-            perror("bind");
-            return false;
-        }
-
-        if (listen(srv, 1) < 0) {
-            perror("listen");
-            return false;
-        }
-
-        std::cout << "Ожидание подключений\n";
-
-        installSignalHandler();
-        prepareSigMasks();
-
-        return true;
-    }
-
-    void run() {
-        while (true) {
-            fd_set rset;
-            FD_ZERO(&rset);
-            FD_SET(srv, &rset);
-            int mx = srv;
-
-            if (client.isActive()) {
-                FD_SET(client.getFd(), &rset);
-                if (client.getFd() > mx) mx = client.getFd();
-            }
-
-            if (got_sighup) {
-                std::cout << "Получен SIGHUP\n";
-                got_sighup = 0;
-            }
-
-            int n = pselect(mx + 1, &rset, nullptr, nullptr, nullptr, &origMask);
-
-            if (n < 0) {
-                if (errno == EINTR) {
-                    std::cout << "Прервано сигналом\n";
+        
+        int ready = pselect(maxFd + 1, &fds, NULL, NULL, NULL, &origMask);
+        
+        if (ready == -1) {
+            if (errno == EINTR) {
+                if (wasSigHup) {
+                    cout << "Received SIGHUP signal" << endl;
+                    wasSigHup = 0;
                     continue;
                 }
+            } 
+            else {
                 perror("pselect");
                 break;
             }
-
-            if (FD_ISSET(srv, &rset)) {
-                handleNewClient();
+        }
+        
+        if (FD_ISSET(listen_fd, &fds)) {
+            struct sockaddr_in client_addr;
+            socklen_t client_len = sizeof(client_addr);
+            int new_client = accept(listen_fd, (struct sockaddr*)&client_addr, &client_len);
+            
+            if (new_client != -1) {
+                char client_ip[INET_ADDRSTRLEN];
+                inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
+                cout << "New connection from " << client_ip << endl;
+                
+                if (client_fd == -1) {
+                    client_fd = new_client;
+                    cout << "Connection accepted" << endl;
+                } 
+                else {
+                    cout << "Closing additional connection" << endl;
+                    close(new_client);
+                }
             }
-
-            if (client.isActive() && FD_ISSET(client.getFd(), &rset)) {
-                client.handleRead();
+        }
+        
+        if (client_fd != -1 && FD_ISSET(client_fd, &fds)) {
+            char buffer[1024];
+            ssize_t bytes = recv(client_fd, buffer, sizeof(buffer), 0);
+            
+            if (bytes > 0) {
+                cout << "Received " << bytes << " bytes" << endl;
+            } 
+            else if (bytes == 0) {
+                cout << "Client disconnected" << endl;
+                close(client_fd);
+                client_fd = -1;
+            } 
+            else {
+                if (errno != EAGAIN && errno != EWOULDBLOCK) {
+                    perror("recv");
+                    close(client_fd);
+                    client_fd = -1;
+                }
             }
         }
     }
-
-    ~Server() {
-        client.closeClient();
-        if (srv != -1) close(srv);
-        std::cout << "Сервер остановлен\n";
-    }
-
-private:
-    int port;
-    int srv;
-    ClientConnection client;
-    sigset_t origMask;
-
-    void installSignalHandler() {
-        struct sigaction act{};
-        act.sa_handler = sigh;
-        act.sa_flags = SA_RESTART;
-        sigaction(SIGHUP, &act, nullptr);
-    }
-
-    void prepareSigMasks() {
-        sigset_t block;
-        sigemptyset(&block);
-        sigaddset(&block, SIGHUP);
-
-        if (sigprocmask(SIG_BLOCK, &block, &origMask) < 0) {
-            perror("sigprocmask");
-            exit(1);
-        }
-    }
-
-    void handleNewClient() {
-        struct sockaddr_in ca{};
-        socklen_t len = sizeof(ca);
-
-        int newfd = accept(srv, (struct sockaddr*)&ca, &len);
-        if (newfd < 0) {
-            perror("accept");
-            return;
-        }
-
-        char ip[16];
-        inet_ntop(AF_INET, &ca.sin_addr, ip, sizeof(ip));
-        std::cout << "Новый клиент " << ip << ":" << ntohs(ca.sin_port)
-                  << " (сокет " << newfd << ")\n";
-
-        if (client.isActive()) {
-            std::cout << "Уже есть клиент — новое соединение закрыто\n";
-            close(newfd);
-        } else {
-            client.setFd(newfd);
-            std::cout << "Клиент подключен\n";
-        }
-    }
-};
-
-int main() {
-    Server server(PORT);
-    if (!server.init()) {
-        std::cerr << "Ошибка инициализации сервера\n";
-        return 1;
-    }
-    server.run();
-    return 0;
+    
+    if (client_fd != -1) close(client_fd);
+    close(listen_fd);
+    return EXIT_SUCCESS;
 }
-
